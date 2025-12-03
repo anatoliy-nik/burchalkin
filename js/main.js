ecbcd8a689cc34b35e889fe01b1ce84eaf6a42f7
anatoliy-nik <dessid@yandex.ru>
2025-12-03 12:58:06 +0300

    начал делать слайдер новостей

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 $(function(){
 
     // бургер кнопка и меню
-    $('.burger-btn').on('click', function() {
-        $('.menu__list').toggleClass('menu__list--active');
-        $('.burger-btn').toggleClass('burger-btn--active');
+    // $('.burger-btn').on('click', function() {
+    //     $('.menu__list').toggleClass('menu__list--active');
+    //     $('.burger-btn').toggleClass('burger-btn--active');
+    // });
+
+    $('.news-list').slick({
+        arrows: true,
+        dots: true,
+        infinite: false,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        autoplay: false
     });
 
 });
