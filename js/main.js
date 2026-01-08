cd9399ac569f78327e84ba70010e9d2db2e7cc30
anatoliy-nik <dessid@yandex.ru>
2026-01-08 12:25:37 +0300

    доделаладаптив слайдера офис

--- js/main.js
+++ js/main.js
@@ -15,11 +15,6 @@ $(function(){
         $btn.toggleClass('open');
     });
 
-    // нужно инициализировать до Slick, чтобы можно было удалить клоны из Fancybox
-    Fancybox.bind("[data-fancybox]", {
-    // Your custom options
-    });
-
     $('.s-news .news-list').slick({
         arrows: true,
         dots: true,
@@ -39,34 +34,73 @@ $(function(){
 
     // чтобы Fancybox не дублировал клонированные слайды (фото), 
     // нужно удалить data-fancybox у клонов сразу после инициализации Slick
-    $('.office').on('init', function() {
-    $('.slick-cloned [data-fancybox]').removeAttr('data-fancybox');
-    }).slick({
-        arrows: true,
-        dots: false,
-        infinite: true,
-        slidesToShow: 6,
-        slidesToScroll: 1,
-        variableWidth: true,
-        autoplay: false,
-        responsive: [
-            {
-            breakpoint: 768,
-            settings: {
-                centerMode: true,
-                arrows: false
-                // slidesToShow: 1,
-                // variableWidth: false,
-                // если не указать, не будет работать 1 слайд
-            }
-            }
-        ]
-    });
+    // $('.office').on('init', function() {
+    // $('.slick-cloned [data-fancybox]').removeAttr('data-fancybox');
+    // }).slick({
+    //     arrows: true,
+    //     dots: false,
+    //     infinite: true,
+    //     slidesToShow: 6,
+    //     slidesToScroll: 1,
+    //     variableWidth: true,
+    //     autoplay: false,
+    //     responsive: [
+    //         {
+    //         breakpoint: 768,
+    //         settings: {
+    //             centerMode: true,
+    //             arrows: false
+    //             // slidesToShow: 1,
+    //             // variableWidth: false,
+    //             // если не указать, не будет работать 1 слайд
+    //         }
+    //         }
+    //     ]
+    // });
 
-    // модальное окно
+    // модальное окно для формы связи
     $('[data-modal]').on('click', function() {
       $($(this).data('modal')).modal();
       return false;
+    });
+
+    new Splide('#office-slider', {
+        type: 'slide',      // ← не 'loop'!
+        rewind: true,       // ← включает "возврат к началу"
+        perPage: 6, // Перебивает стили!
+        // focus: 0, // чтобы в dots было разбиение по слайдам (по дефолту - по странице)
+        perMove: 1, 
+        // drag: 'free',
+        gap: 24, // подогнать и прописать именно здесь, а не в стилях. Перебивает стили!
+        pagination: false,
+        speed: 500, // по дефолту 400
+        rewindSpeed: 500,
+        breakpoints: {
+            1399: {
+                perPage: 5,
+            },
+            1199: {
+                perPage: 4,
+            },
+            991: {
+                perPage: 3,
+            },
+            767: {
+                perPage: 2,
+            },
+            539: {
+                gap: 12,
+                pagination: true,
+                arrows: false,
+                grid: { rows: 2, cols: 1, gap: {row: 12} },
+                perPage: 2,
+                perMove: 1,
+            }
+        }
+    }).mount(window.splide.Extensions);
+    
+    Fancybox.bind("[data-fancybox]", {
+    // Your custom options
     });
 
 });
